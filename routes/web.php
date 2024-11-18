bf1346e10587764cb165fa2a4b3ad4bd8aef802a
mukeshkumarchoudhary <mukeshkadwa2021@gmail.com>
2024-11-18 11:39:19 +0530

    login page route change

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,18 @@ use App\Http\Controllers\Admin\PaymentsController;
 use App\Http\Controllers\Admin\ReviewRatingController;
 use App\Http\Controllers\Admin\VehicleTypeController;
 
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Auth/Login');
 });
 
 Route::get('/dashboard', function () {
