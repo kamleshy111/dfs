29b2ccfe92ee7625c0a6ac902734c4ddeb68d2ce
mukeshkumarchoudhary <mukeshkadwa2021@gmail.com>
2024-12-07 11:12:07 +0530

    vue device update

--- resources/js/Pages/Device/View.vue
+++ resources/js/Pages/Device/View.vue
@@ -21,7 +21,7 @@ const goBack = () => {
 
     <div class="back-section"><button type="button" class="back-btn-custom" @click="goBack"><i class="bi bi-caret-left"></i> Back</button></div>
 
-    <div class="py-3">
+    <div class="py-3 row">
         <div class="profile-card col-md-6 col-12">
             <div class="profile-details mt-3 row">
                 <div class="col-md-6 col-12">
@@ -30,6 +30,28 @@ const goBack = () => {
                 <p class="mt-2">Company Name: {{ props.devices.company_name }}</p>
                 <p class="mt-2">Date : {{ props.devices.date_time }}</p>
                 <p  class="mt-2"><strong>invoice_photos:</strong><img width="150" class="p-4" :src="props.devices.invoice_photos" alt="Invoice photos"></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="profile-card col-md-6 col-12">
+            <div class="profile-details mt-3 row">
+                <div class="col-md-6 col-12">
+                  <strong>Vehicle Information</strong>
+                <h4 class="mt-2">Customer Name: {{ props.devices.customerFirstName }} {{ props.devices.customerLsatName }}</h4>
+                
+                <p class="mt-2">Vehicle Type : {{ props.devices.vehicleType }}</p>
+                <p class="mt-2">Vehicle Name: {{ props.devices.vehicleName }}</p>
+                <p class="mt-2">Vehicle Register Number: {{ props.devices.vehicleRegisterNumber }}</p>
+
+                <p class="mt-2">SIM Operator : {{ props.devices.simOperator }}</p>
+                <p class="mt-2">SIM Card Number : {{ props.devices.simCardNumber }}</p>
+                <p class="mt-2">IMEI Number : {{ props.devices.imeiNumber }}</p>
+                
+                <p class="mt-2">Installation Date : {{ props.devices.installationDate }}</p>
+                <p class="mt-2">Start Date : {{ props.devices.startDate }}</p>
+                <p class="mt-2">Duration : {{ props.devices.duration }} {{ props.devices.durationUnit }}</p>
+                <p class="mt-2">Expiration Date : {{ props.devices.expirationDate }}</p>
                 </div>
             </div>
         </div>
